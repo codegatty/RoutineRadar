a047c914f31878b61a208652e2c9d0649bd557cf
codegatty <rajithgatty61@gmail.com>
2024-06-11 16:50:07 +0530

    minor update in analytics changed ip

--- backend/admin_service/routes/countRoute.js
+++ backend/admin_service/routes/countRoute.js
@@ -6,15 +6,17 @@ const axios=require("axios");
 
 
 const tokenValidator = require('../middleware/tokenValidator');
+const challenge_service_ip="172.18.0.6:5001"
+const user_service_ip="172.18.0.7:5002"
 
 router.get('/',tokenValidator,asyncHandler( async (req,res)=>{
     //admin Count
     const adminCount=await Admin.estimatedDocumentCount();
 
     //user count
-    const request1=axios.get("http://localhost:5001/challenge/count");
+    const request1=axios.get(`http://${challenge_service_ip}/challenge/count`);
     //challenge count
-    const request2=axios.get("http://localhost:5002/user/counts")
+    const request2=axios.get(`http://${user_service_ip}/user/counts`)
 
     Promise.all([request1,request2]).then((responses)=>{
         const [response1,response2]=responses
@@ -28,8 +30,6 @@ router.get('/',tokenValidator,asyncHandler( async (req,res)=>{
         console.log(er);
         return res.status(400).json({message:"bad request"})
     })
-
-
     
 }));
 
